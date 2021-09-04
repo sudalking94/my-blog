26a371004339e8bcdaeb63ea49b6244d8a76256a
junwo <loust3598@naver.com>
2021-09-04 21:50:14 +0900

    홈화면 ui작업 중

--- frontend/src/Routes/Home/HomePresenter.js
+++ frontend/src/Routes/Home/HomePresenter.js
@@ -1,7 +1,39 @@
 import React from "react";
+import mypic from "../../static/images/전준우.jpg";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  padding-top: 50px;
+  display: grid;
+
+  grid-template-columns: 1fr 2fr;
+`;
+
+const MyPicContainer = styled.div`
+  background-color: green;
+`;
+
+const MyPic = styled.img`
+  border: 1px solid black;
+  width: 270px;
+  height: 270px;
+  object-fit: contain;
+  border-radius: 100%;
+`;
+
+const Biography = styled.div`
+  background-color: blue;
+`;
 
 const HomePresenter = () => {
-  return <div>homepresenter</div>;
+  return (
+    <HomeContainer>
+      <MyPicContainer>
+        <MyPic src={mypic}></MyPic>
+      </MyPicContainer>
+      <Biography></Biography>
+    </HomeContainer>
+  );
 };
 
 export default HomePresenter;
